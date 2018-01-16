54076ef204d7096d101e08d7ffa9bba1424598a7
fang duan <df1228@gmail.com>
2018-01-16 18:49:57 +0800

    Add more endpoints

--- server/controllers/mr.js
+++ server/controllers/mr.js
@@ -1,32 +1,46 @@
+import { log } from "util";
+
 var PostgREST = require("postgrest-client")
-var Api = new PostgREST("http://babycare.natapp1.cc");
+var Api = new PostgREST("http://babycare.natapp1.cc")
 
 async function getOne(ctx, next) {
+  const id = ctx.querystring.id
+  const data = await Api.get(`/fa_medical_record?id=eq.$id`)
+  ctx.state.data = {
+    medical_record: data
+  }
 }
 
 async function getAll(ctx, next) {
-  var data = await Api.get('/fa_medical_record')
+  const data = await Api.get('/fa_medical_record')
   // ctx.state.data = { "helll": "world"}
-  // console.log('sdfasdfasdfasdfasd')
-  // console.log(data)
+  console.log(data)
   ctx.state.data = {
     medical_records: data
   }
 }
 
-// getAll()
-// Api.get("/fa_medical_record").then(data => console.log(data))
-
 async function post(ctx, next) {
-
+  const data = await Api.post(`/fa_medical_record`)
+  // ctx.state.data = { "helll": "world"}
+  // console.log('sdfasdfasdfasdfasd')
+  console.log(data)
+  ctx.state.data = {
+    status: 'OK'
+  }
 }
 
 async function update(ctx, next) {
-
+  const data = await Api.patch('/fa_medical_record')
+  console.log(data);
+  ctx.state.data = {
+    status: "OK"
+  };
 }
 
 async function destroy(ctx, next) {
-
+  // const data = await Api.delete('/fa_medical_record')
+  // ctx.state.data = { medical_records: data };
 }
 
 module.exports = {
